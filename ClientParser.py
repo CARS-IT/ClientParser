027f56cd83b3cea442a9257d6ad0a6739414ffbc
Christofanis Skordas <55315750+skordaschristofanis@users.noreply.github.com>
2025-04-01 09:48:24 -0500

    adds command line arguments

--- ClientParser.py
+++ ClientParser.py
@@ -12,14 +12,23 @@
 # Copyright (C) 2025 NSF SEES, USA
 # ----------------------------------------------------------------------------------
 
+import argparse
 from clientparser import ClientParser
 
 
 def main() -> None:
     """Main entry point for the Client Parser application."""
-    # Create the client parser instance and run the application
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser(description="Client Parser")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
+    parser.add_argument("-i", "--interval", type=int, default=0, help="Set the interval in seconds. If the value is 0, the application will run once and exit")
+    args = parser.parse_args()
+
+    # Create the client parser instance
     client_parser = ClientParser()
-    client_parser.run()
+
+    # Run the client parser with the specified arguments
+    client_parser.run(verbose=args.verbose, interval=args.interval)
 
 
 if __name__ == "__main__":
